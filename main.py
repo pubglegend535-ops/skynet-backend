52028466f47c3cbe37bb4b62f204e345db31d648
pubglegend535-ops <pubglegend535@gmail.com>
2026-07-24 17:18:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import os
+import google.genai as genai
 
 app = FastAPI()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 @app.get("/")
 def root():
     return {"message": "Hello Skynet"}
+
+@app.get("/ask")
+def ask(q: str):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=q
+    )
+    return {"reply": response.text}
